714eb387cfc67f931ece5adc77af476b344e209c
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-02 14:02:56 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,13 @@ i = 0
 block_return_values = []
 while i < collection.length 
 block_return_values << yield(collection[i])
-yield(collection[i])
 i += 1 
 end 
+if 
+  block_return_values.include?
+  (false)
+  false
+else
+  true
+end
 end
